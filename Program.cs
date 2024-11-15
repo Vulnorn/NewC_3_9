9299e0f785dc0b43cab91c74255d6578cceb8c40
Vulnorn <mixadrbk31@gmail.com>
2024-11-15 09:44:42 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Mail;
 
 namespace NewC_3_9
 {
@@ -10,42 +9,24 @@ namespace NewC_3_9
             char openingBracket = '(';
             char closingBracket = ')';
             int correctExpression = 0;
+            int currentRepeatCount = 0;
+            int deep = 0;
 
-            char[] symbols = new char[] { '(', '(', ')',')' };
+            char[] symbols = new char[] { '(', '(', ')', ')' };
 
             for (int i = 0; i < symbols.Length; i++)
             {
                 Console.Write($"{symbols[i]}");
             }
 
+            char currentChar = symbols[0];
+
             for (int i = 0; i < symbols.Length; i++)
             {
                 if (symbols[i] == openingBracket)
                 {
                     correctExpression--;
-                }
-                else if (symbols[i] == closingBracket)
-                {
-                    correctExpression++;
-                    if (correctExpression == 1)
-                    {
-                        break;
-                    }
-                }
-            }
 
-            char currentChar = symbols[0];
-            int currentRepeatCount = 0;
-            int deep = 0;
-
-            if (correctExpression != 0)
-            {
-                Console.WriteLine($"\nСкобочное выражение - некорректное");
-            }
-            else
-            {
-                for (int i = 0; i < symbols.Length; i++)
-                {
                     if (symbols[i] == currentChar)
                     {
                         currentRepeatCount++;
@@ -61,9 +42,22 @@ namespace NewC_3_9
                         currentRepeatCount = 1;
                     }
                 }
+                else if (symbols[i] == closingBracket)
+                {
+                    correctExpression++;
 
-                Console.WriteLine($"\nСкобочное выражение - корректное, глубина равна {deep}");
+                    if (correctExpression > 0)
+                    {
+                        Console.WriteLine($"\nСкобочное выражение - некорректное");
+                        break;
+                    }
+                }
             }
+
+            if (correctExpression != 0)
+                Console.WriteLine($"\nСкобочное выражение - некорректное");
+            else
+                Console.WriteLine($"\nСкобочное выражение - корректное, глубина равна {deep}");
 
             Console.ReadKey();
         }
